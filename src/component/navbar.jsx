fb5e5460054d1ae38f13d59990be91934d52859a
Mohd Kamleen <58457138+mohdkamleen@users.noreply.github.com>
2022-11-25 01:59:49 +0400

    change logo name

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -9,7 +9,7 @@ function navbar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="#">BackstopIndia</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
