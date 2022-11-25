2ed0cdf0b6e4598b4f16ce9708090af2b9995171
Mohd Kamleen <58457138+mohdkamleen@users.noreply.github.com>
2022-11-25 18:13:11 +0400

    add image in front page

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -18,20 +18,9 @@ function navbar() {
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <Nav.Link href="#action2">About</Nav.Link>  
+            <Nav.Link href="#action2">Contact</Nav.Link>  
+            <Nav.Link href="#action2">Services</Nav.Link>  
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -40,7 +29,7 @@ function navbar() {
               className="me-2"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-secondary">Search</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import Navbar from '../component/navbar'
+import Navbar from '../component/navbar' 
 
 const home = () => {
   return (
     <>
-      <Navbar />
+      <Navbar /> 
+      <section className='banner' style={{background:"url(assest/image/crack-phone.jpg)"}}>
+        <h3>Lorem ipsum dolor sit amet.</h3>
+        <div>
+          
+        </div>
+      </section>
     </>
   )
 }
