1a61b031a36d8703919449a9fa3d89cd59d4aa02
mohdkamleen <kamleenmohd@gmail.com>
2022-11-26 23:28:19 +0530

    sdf

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'  
 import { Link } from 'react-router-dom';
 function Footer() {
-    return (
+    return ( 
         <> 
             <div className='footer-section'>
                  <div>
