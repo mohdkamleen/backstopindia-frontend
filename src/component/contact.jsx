e813d65c951f4fd544d921f6f88d64625622375c
Mohd Kamleen <58457138+mohdkamleen@users.noreply.github.com>
2022-11-29 22:05:00 +0400

    Merge branch 'main' of https://github.com/mohdkamleen/backstopindia-frontend

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button, Form } from 'react-bootstrap';
-import {FaLocationArrow, FaMailBulk, FaPhone, FaSearchLocation} from 'react-icons/fa'
+import { FaMailBulk, FaPhone, FaSearchLocation} from 'react-icons/fa'
 function Contact() {
     return (
         <>
