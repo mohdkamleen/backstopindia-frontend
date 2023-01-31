940b24658064111b78137ced9201883a6f9786b4
mohdkamleen <kamleenmohd@gmail.com>
2023-01-31 19:02:46 +0530

    done

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,13 +1,7 @@
-import axios from 'axios'; 
-// const https = require('https')
-
-// const agent = new https.Agent({
-//     rejectUnauthorized: false,
-// });
+import axios from 'axios';  
  
 export default axios.create({ 
-  baseURL: "http://43.205.213.166:8000/api", //for production mode
-  httpsAgent:false,
+  baseURL: "http://43.205.213.166:8000/api", //for production mode 
   headers: {
     "Content-type": "application/json"
   }
